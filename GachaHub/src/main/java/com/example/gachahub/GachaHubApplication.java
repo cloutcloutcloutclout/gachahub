c89bfc818cde49a7ded7dcc443fff699f5aab7d5
Zain <zainhu06@hotmail.com>
2026-03-01 17:40:30 +0000

    Created Project

---
+++ GachaHub/src/main/java/com/example/gachahub/GachaHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.gachahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachaHubApplication.class, args);
+	}
+
+}
